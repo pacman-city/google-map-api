7a1672be304242d385fd8808ba0236e76a91f1ba
evgeni <kluev.evga@gmail.com>
2022-07-29 02:08:19 +0300

    interface in axios response example, basic ts usage

---
+++ 01 fetch-json/index.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+interface Todo {
+   id: number;
+   title: string;
+   completed: boolean;
+}
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1'
+
+axios.get(url).then(res => {
+   const todo = res.data as Todo;
+
+   const id = todo.id;
+   const title = todo.title;
+   const completed = todo.completed;
+
+   logTodo(id, title, completed);
+})
+
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+   console.log(`
+         The Todo with ID: ${id}
+         Has a title of: ${title}
+         Is it completed: ${completed}
+      `);
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+#### Используется tsc & ts-node
+
+npm i -g ts-node
+
+ts-node <filename.ts> - скомпилировать и выполнить
